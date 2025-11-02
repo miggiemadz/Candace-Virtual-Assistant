21553b98a34156dd73db3001786f92f104192632
Miggie <miguelt613377@gmail.com>
2025-11-02 16:01:11 -0500

    Add basic login and dashboard routes
    Introduces initial user login, dashboard, and sign-up routes in routes.py. Adds placeholder variables and a stub for login credential verification in main.py to support future authentication logic.

--- Candace Virtual Assistant/app/main.py
+++ Candace Virtual Assistant/app/main.py
@@ -1,4 +1,12 @@
-import routes
+import routes, sqlite3
+
+
+
+userNameInput = ""
+passwordInput = ""
+
+def VerifyLoginCredentials(username: str, password: str) -> bool: 
+
 
 if __name__ == '__main__':
     routes.RunApp()
--- Candace Virtual Assistant/app/routes.py
+++ Candace Virtual Assistant/app/routes.py
@@ -1,10 +1,21 @@
-from flask import Flask, app
+from flask import Flask, redirect, url_for, request, app
+import main
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Candace is Running"
+@app.route('/', methods=['GET', 'POST'])
+def UserLoginPage():
+    if main.accountVerified:
+        return redirect(url_for('StudentLoginPage'))
+    return "Main dashboard."
+
+@app.route('/dashboard')
+def DashboardPage():
+    return "This is the student dashboard page."
+
+@app.route('/student-login/sign-up')
+def StudentSignUpPage():
+    return "This is the student sign up page."
 
 def RunApp():
     return app.run(debug=True)
