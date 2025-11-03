3274338c37cdb5863255f639a03c2c622d1b6142
Miggie <miguelt613377@gmail.com>
2025-11-02 21:26:17 -0500

    Add login page and update authentication flow
    Introduces a login-page.html template for user authentication. Refactors routes and main logic to handle login form submission, credential verification, and error display. Removes unused empty.py file.

--- Candace Virtual Assistant/app/main.py
+++ Candace Virtual Assistant/app/main.py
@@ -1,12 +1,10 @@
-import routes, sqlite3
-
-
+import routes
 
 userNameInput = ""
 passwordInput = ""
 
-def VerifyLoginCredentials(username: str, password: str) -> bool: 
-
+def VerifyLoginCredentials(username: str, password: str, db) -> bool: 
+    return username == "0679866" and password == "Miggie(2004)"
 
 if __name__ == '__main__':
     routes.RunApp()
--- Candace Virtual Assistant/app/routes.py
+++ Candace Virtual Assistant/app/routes.py
@@ -1,13 +1,20 @@
-from flask import Flask, redirect, url_for, request, app
-import main
+from flask import Flask, redirect, url_for, request, app, render_template
+import main, db_utils
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def UserLoginPage():
-    if main.accountVerified:
-        return redirect(url_for('StudentLoginPage'))
-    return "Main dashboard."
+    if request.method == 'POST':
+        db = db_utils.get_db()
+        username_input = request.form['username']
+        password_input = request.form['password']
+        if (main.VerifyLoginCredentials(username_input, password_input, db)):
+            return redirect(url_for('DashboardPage'))
+        else:
+            return render_template('login-page.html', error="Invalid credentials")
+    else:
+        return render_template('login-page.html')
 
 @app.route('/dashboard')
 def DashboardPage():
