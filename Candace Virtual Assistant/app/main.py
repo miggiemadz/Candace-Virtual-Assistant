a1912ae0b68a665dbf132a3a66a1d5a492837f05
Miggie <miguelt613377@gmail.com>
2025-11-19 10:44:01 -0500

    Add initial password validation and update signup check
    Introduced a ValidatePassword function to begin password validation logic and updated VerifySignUpCredentials to check for existing usernames. The signup function now returns a tuple with an error message if the username is already in use.

--- Candace Virtual Assistant/app/main.py
+++ Candace Virtual Assistant/app/main.py
@@ -7,8 +7,29 @@ def VerifyLoginCredentials(username: str, password: str, db: sqlite3.Connection)
     return cur.fetchone() != None
     
 
-def VerifySignUpCredentials(student_id: int, username: str, password: str, db: sqlite3.Connection) -> bool:
+def VerifySignUpCredentials(student_id: int, username: str, password: str, db: sqlite3.Connection):
+    cur = db.cursor()
+    id_check = cur.execute("SELECT * FROM LOGIN_INFO WHERE login_id = ?", username)
+    if id_check.fetchone() is not None:
+        return False, "Student email already in use."
+    else:
+        return False
+    
     return False
+
+def ValidatePassword(password: str) -> bool:
+    has_number = False
+    has_special_character = False
+    has_character_count = False
+
+    pass_in_chars = list(password)
+    has_character_count = pass_in_chars.count >= 8
+
+    for p in pass_in_chars:
+        try:
+            return int(p)
+        except ValueError:
+            return None
 
 
 if __name__ == '__main__':
